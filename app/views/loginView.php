462d356bf4a113f1276aee97c2c61228837c9003
jawinga <cristianharders@gmail.com>
2025-03-14 18:02:24 +0100

    update

---
+++ app/views/loginView.php
@@ -0,0 +1,18 @@
+<h1 class="titulo__inicioSesion">Inicia sesión con tu cuenta</h1>
+
+<form action="#" method="POST" class="titulo__inicioSesion">
+
+<label for="email" class="label__inicioSesion">Correo electrónico</label>
+<input type="email" id="email" name="email" class="input__inicioSesion" required>
+
+<label for="password" class ="label__inicioSesion">Contraseña</label>
+<input type="password" id="password" name="password" class="input__inicioSesion" required>
+
+<button type="submit" class="boton__inicioSesion">Iniciar sesión</button>
+
+<p>¿No tienes una cuenta? <a href="#" class="enlace">Regístrate aquí</a></p>
+
+</form>
+
+
+
